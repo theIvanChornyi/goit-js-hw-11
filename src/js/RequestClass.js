203d20740ce0f74e88b5f0849642a4b2a0943d61
Ivan Chornyi <chorniyivan@icloud.com>
2022-09-08 18:24:26 +0300

    completed

---
+++ src/js/RequestClass.js
@@ -0,0 +1,34 @@
+const axios = require('axios').default;
+export default class NewItems {
+  constructor() { 
+    let userData = '';
+    let pagination = 1;
+  }
+  async request() {
+     return await axios({
+      url: 'https://pixabay.com/api/',
+      method: 'get',
+      params: {
+        key: '29712596-a6647cd6fa13e8a799ad6d26d',
+        q: this.userData,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: 40,
+        page: this.pagination,
+       }
+     });
+  }
+  get search() {
+    return this.userData;
+  }
+  set search(value) {
+    this.userData = value;
+  }
+  updatePage() {
+    this.pagination += 1;
+  }
+  resetPage() {
+    this.pagination = 1;
+  }
+}
---
+++ src/js/index.js
@@ -0,0 +1,118 @@
+import '../styles.css';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import Newitems from '../js/RequestClass';
+import InfiniteScroll from 'infinite-scroll';
+
+const searchingForm = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+const showMoreBtn = document.querySelector('.load-more');
+
+const getItems = new Newitems();
+const lightbox = new SimpleLightbox('.gallery a',{  captions: true,
+  captionType: 'attr',
+  captionsData:	'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  docClose: true,
+});
+
+searchingForm.addEventListener('submit', initialGalleryEl);
+showMoreBtn.addEventListener('click', addGalleryEl);
+gallery.addEventListener('click', (e) => e.preventDefault());
+
+async function initialGalleryEl(event) {
+  event.preventDefault();
+  getItems.search = event.target.searchQuery.value.trim();
+  getItems.resetPage();
+  const cardsAmound = await createElems(parseHtml);
+  if (cardsAmound > 0) {
+    await Notify.info(`Hooray! We found ${cardsAmound} images.`);
+    await apearBtn(showMoreBtn);
+  } 
+}
+
+async function addGalleryEl() {
+  await createElems(insertHtml); 
+  await slowScroll(gallery);
+}
+
+function getGalleryHtml(Array) {
+  return Array.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+    return `<a class="photo-card post" href="${largeImageURL}">
+              <img
+                class="gallery-image"
+                src="${webformatURL}"
+                alt="${tags}"
+                loading="lazy"
+              />
+              <div class="info">
+                <p class="info-item">
+                  <b>Likes</b>
+                  ${likes}
+                </p>
+                <p class="info-item">
+                  <b>Views</b>
+                  ${views}
+                </p>
+                <p class="info-item">
+                  <b>Comments</b>
+                  ${comments}
+                </p>
+                <p class="info-item">
+                  <b>Downloads</b>
+                  ${downloads}
+                </p>
+              </div>
+            </a>`;
+   }).join('');
+}
+
+async function createElems(callback) {
+  try {
+    const {hits: dataCard ,total:totalCards} = await (await getItems.request()).data;
+    if (dataCard.length < 1) { 
+      destroyHtml(gallery);
+      disappearBtn(showMoreBtn);
+      Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+      return;
+    }
+    const Htmlstring = await getGalleryHtml(dataCard);
+    await callback(Htmlstring);
+    getItems.updatePage();
+    lightbox.refresh();
+    return totalCards;
+  } catch (error) {
+    Notify.failure(error.mesage);
+  }
+}
+
+function parseHtml(htmlstring) {
+  gallery.innerHTML = htmlstring;
+}
+
+function insertHtml(htmlstring) {
+  gallery.insertAdjacentHTML("beforeend",htmlstring);
+}
+
+function destroyHtml(where) {
+  where.innerHTML = '';
+}
+
+function slowScroll(where) {
+  const { height } = where.getBoundingClientRect();
+  let scrollValue = 0;
+  window.scrollBy({
+    top: scrollValue +=height,
+    behavior: "smooth"
+  });
+}
+
+function apearBtn(where) {
+  where.classList.remove('visually-hidden');
+}
+
+function disappearBtn(where) {
+  where.classList.add('visually-hidden');
+}
