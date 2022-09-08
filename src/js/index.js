0fd02c98f217c546d82970aa7a68a1d125192eaf
Ivan Chornyi <chorniyivan@icloud.com>
2022-09-08 18:31:40 +0300

    change css file way

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,9 @@
-import '../styles.css';
+import './css/styles.css';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import Newitems from '../js/RequestClass';
-import InfiniteScroll from 'infinite-scroll';
+// import InfiniteScroll from 'infinite-scroll';
 
 const searchingForm = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
